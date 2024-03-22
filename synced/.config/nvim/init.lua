61be08ea9233e3e08b29c6386389148df60ebf1c
Tronikelis <contactdonatas@gmail.com>
2024-03-22 18:24:59 +0200

    rose pine

--- synced/.config/nvim/init.lua
+++ synced/.config/nvim/init.lua
@@ -15,6 +15,47 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     "ThePrimeagen/vim-be-good",
+    { 
+        "rose-pine/neovim",
+        priority = 1000,
+        init = function()
+            vim.cmd.colorscheme 'rose-pine'
+        end
+    },
+    { 
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            auto_install = true,
+            ensure_installed = {
+                "bash",
+                "c",
+                "diff",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "jsonc",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+            },
+        },
+    }
 })
 
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
