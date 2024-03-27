4a6d4c456cd98263093c97dd6c17aab83033214c
Tronikelis <contactdonatas@gmail.com>
2024-03-27 18:33:17 +0200

    nvim shits

--- synced/.config/nvim/lua/remaps/init.lua
+++ synced/.config/nvim/lua/remaps/init.lua
@@ -41,3 +41,16 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.diagnostic.config({
 	update_in_insert = true,
 })
+
+local yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
