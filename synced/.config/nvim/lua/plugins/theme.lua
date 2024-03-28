83a233995986f4b69f9cc0318e53647f30dc4141
Tronikelis <contactdonatas@gmail.com>
2024-03-28 21:38:21 +0200

    telescope 016

--- synced/.config/nvim/lua/plugins/theme.lua
+++ synced/.config/nvim/lua/plugins/theme.lua
@@ -1,16 +1,15 @@
 return {
-    "catppuccin/nvim", 
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            integrations = {
-                treesitter = true,
-                cmp = true,
-                gitsigns = true,
-            }
-        })
+	"catppuccin/nvim",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			integrations = {
+				treesitter = true,
+				cmp = true,
+				gitsigns = true,
+			},
+		})
 
-        vim.cmd.colorscheme "catppuccin"
-    end,
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
-
