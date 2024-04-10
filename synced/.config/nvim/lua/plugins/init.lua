6d084fef74360572a0aad2a0f3d62810f3ae7b5d
Tronikel <contactdonatas@gmail.com>
2024-04-10 18:13:15 +0300

    just in case

--- synced/.config/nvim/lua/plugins/init.lua
+++ synced/.config/nvim/lua/plugins/init.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
 	"tpope/vim-sleuth",
 	{
 		"windwp/nvim-autopairs",
+		dependencies = "nvim-treesitter/nvim-treesitter",
 		event = "InsertEnter",
 		config = function()
 			require("nvim-autopairs").setup({
