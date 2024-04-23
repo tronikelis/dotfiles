555ed9e02d2537daa4a6931206bf2fa715e14d86
Tronikel <contactdonatas@gmail.com>
2024-04-23 22:29:05 +0300

    show ip

--- synced/.config/nvim/lua/plugins/lualine.lua
+++ synced/.config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,26 @@
+local private_ip_addr = ""
+local update_private_ip
+update_private_ip = function()
+	vim.fn.jobstart(
+		'ip addr show | grep "192\\.\\|10\\." | head -1 | awk \'{split($0, a, " "); print a[2]}\' | cut -f1 -d"/" | tr -d \'[:blank:]\\n\'',
+		{
+			stdout_buffered = true,
+			detach = false,
+			on_stdout = function(chan_id, stdout)
+				private_ip_addr = stdout[1]
+			end,
+			on_exit = function()
+				vim.defer_fn(update_private_ip, 10000)
+			end,
+		}
+	)
+end
+update_private_ip()
+
+local get_private_ip_addr = function()
+	return private_ip_addr
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -12,7 +35,7 @@ return {
 				},
 				lualine_a = {},
 				lualine_c = {},
-				lualine_x = {},
+				lualine_x = { get_private_ip_addr },
 				lualine_y = {},
 				lualine_z = {
 					{
