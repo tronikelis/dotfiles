570319de991f049999f0e9437ccfffdd05759102
Tronikel <contactdonatas@gmail.com>
2024-04-24 20:26:25 +0300

    wttr in weather

--- synced/.config/nvim/lua/plugins/lualine.lua
+++ synced/.config/nvim/lua/plugins/lualine.lua
@@ -21,6 +21,27 @@ local get_private_ip_addr = function()
 	return private_ip_addr
 end
 
+local weather = ""
+local update_weather
+update_weather = function()
+	vim.fn.jobstart('curl -s "wttr.in/Vilnius?format=%c%t" | tr -d "[:blank:]"', {
+		detach = false,
+		stdout_buffered = true,
+		on_stdout = function(chan_id, stdout)
+			weather = stdout[1]
+		end,
+		on_exit = function()
+			local mins30 = 1000 * 60 * 30
+			vim.defer_fn(update_weather, mins30)
+		end,
+	})
+end
+update_weather()
+
+local get_weather = function()
+	return weather
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -36,7 +57,7 @@ return {
 				lualine_a = {},
 				lualine_c = {},
 				lualine_x = { get_private_ip_addr },
-				lualine_y = {},
+				lualine_y = { get_weather },
 				lualine_z = {
 					{
 						"datetime",
