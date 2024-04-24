cea14e305377326e83f52a5eb0d9512e53735f0c
Tronikel <contactdonatas@gmail.com>
2024-04-24 21:06:24 +0300

    remove leader pv mapping

--- synced/.config/nvim/lua/remaps/init.lua
+++ synced/.config/nvim/lua/remaps/init.lua
@@ -33,7 +33,9 @@ vim.opt.termguicolors = true
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", function()
+	print("use -")
+end)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
