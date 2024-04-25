c0ee7a4218f930efa7f3285ff2f7ab0b93022d6c
Tronikel <contactdonatas@gmail.com>
2024-04-25 16:45:23 +0300

    ghost text

--- synced/.config/nvim/lua/plugins/lsp.lua
+++ synced/.config/nvim/lua/plugins/lsp.lua
@@ -84,8 +84,12 @@ return {
 				},
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item({
+					behavior = cmp.SelectBehavior.Select,
+				}),
+				["<C-p>"] = cmp.mapping.select_prev_item({
+					behavior = cmp.SelectBehavior.Select,
+				}),
 
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -113,6 +117,9 @@ return {
 					selection_order = "near_cursor",
 					follow_cursor = true,
 				},
+			},
+			experimental = {
+				ghost_text = true,
 			},
 		})
 
