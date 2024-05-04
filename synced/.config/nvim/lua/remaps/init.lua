94400bf89bcf97dfa123a1612da48c0daf06613e
Tronikel <contactdonatas@gmail.com>
2024-05-04 11:19:11 +0300

    folding

--- synced/.config/nvim/lua/remaps/init.lua
+++ synced/.config/nvim/lua/remaps/init.lua
@@ -72,3 +72,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		})
 	end,
 })
+
+-- folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldlevelstart = 99
