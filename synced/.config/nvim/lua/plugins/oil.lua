5ef405baae7af1acca5b4525fe7f37f62dfe5e52
Tronikelis <contactdonatas@gmail.com>
2024-05-23 09:42:44 +0300

    dont set root auto

--- synced/.config/nvim/lua/plugins/oil.lua
+++ synced/.config/nvim/lua/plugins/oil.lua
@@ -28,17 +28,5 @@ return {
 
 		vim.keymap.set("n", "-", "<cmd>Oil<cr>")
 		vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
-
-		-- https://www.reddit.com/r/neovim/comments/1cwd181/comment/l4wqmza/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-		-- sets cwd to git dir
-		vim.api.nvim_create_autocmd("BufEnter", {
-			callback = function(ctx)
-				local dir = require("oil").get_current_dir() or ctx.buf
-				local root = vim.fs.root(dir, { ".git" })
-				if root then
-					vim.uv.chdir(root)
-				end
-			end,
-		})
 	end,
 }
