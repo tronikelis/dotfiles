3d3210448352a42f9d44a7f404ced35f3761c20f
Tronikel <contactdonatas@gmail.com>
2024-08-28 15:19:48 +0300

    oil cleanup

--- synced/.config/nvim/lua/plugins/oil.lua
+++ synced/.config/nvim/lua/plugins/oil.lua
@@ -2,25 +2,10 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("oil").setup({
-			keymaps = {
-				["g?"] = false,
-				["<cr>"] = "actions.select",
-				["<C-s>"] = false,
-				["<C-h>"] = false,
-				["<C-t>"] = false,
-				["<C-p>"] = false,
-				["<C-c>"] = false,
-				["<C-l>"] = false,
-				["-"] = "actions.parent",
-				["_"] = false,
-				["`"] = false,
-				["~"] = false,
-				["gs"] = false,
-				["gx"] = false,
-				["g."] = false,
-				["g\\"] = false,
-			},
+		local oil = require("oil")
+
+		oil.setup({
+			use_default_keymaps = false,
 			view_options = {
 				show_hidden = true,
 				is_always_hidden = function(name)
@@ -30,7 +15,19 @@ return {
 			watch_for_changes = true,
 		})
 
+		local actions = require("oil.actions")
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "oil",
+			callback = function(event)
+				local bufnr = event.buf
+
+				vim.keymap.set("n", "<Cr>", actions.select.callback, { buffer = bufnr })
+				vim.keymap.set("n", "-", actions.parent.callback, { buffer = bufnr })
+			end,
+		})
+
 		vim.keymap.set("n", "-", "<cmd>Oil<cr>")
-		vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+		vim.keymap.set("n", "<leader>-", oil.toggle_float)
 	end,
 }
