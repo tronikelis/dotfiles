296be202f8a53121cc9b4deee709a7799f5032f3
Tronikel <contactdonatas@gmail.com>
2024-08-29 23:24:01 +0300

    blame line better + branch sort by commit

--- synced/.config/nvim/lua/plugins/gitsigns.lua
+++ synced/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,14 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
+		local blame_fast = { "-w", "-C" }
+		local blame_slow = { table.unpack(blame_fast), "-C", "-C" }
+
 		require("gitsigns").setup({
 			current_line_blame = true,
+			current_line_blame_opts = {
+				extra_opts = blame_fast,
+			},
 
 			on_attach = function(bufnr)
 				local gitsigns = require("gitsigns")
@@ -11,9 +17,11 @@ return {
 				vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, opts)
 				vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, opts)
 
-				vim.keymap.set("n", "<leader>hb", gitsigns.blame_line, opts)
+				vim.keymap.set("n", "<leader>hb", function()
+					gitsigns.blame_line({ extra_opts = blame_slow })
+				end, opts)
 				vim.keymap.set("n", "<leader>hB", function()
-					gitsigns.blame_line({ full = true })
+					gitsigns.blame_line({ full = true, extra_opts = blame_slow })
 				end, opts)
 
 				vim.keymap.set("n", "[h", function()
