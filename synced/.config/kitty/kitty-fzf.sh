f674bd8f32125fbafbe8230e05a97214821c5679
Tronikel <contactdonatas@gmail.com>
2024-09-18 22:45:15 +0300

    minify a bit

--- synced/.config/kitty/kitty-fzf.sh
+++ synced/.config/kitty/kitty-fzf.sh
@@ -8,5 +8,5 @@ all_tabs="$(
     ' | column -ts $'\t'
 )"
 
-new_tab_id="$(fzf --header "Select tab:" --header-first --reverse <<<"${all_tabs}" | awk '{ print $2 }')"
-kitty @ focus-tab --match "id:${new_tab_id}"
+new_tab_id="$(fzf --header "Select tab:" --header-first --reverse <<<"$all_tabs" | awk '{ print $2 }')"
+kitty @ focus-tab --match "id:$new_tab_id"
