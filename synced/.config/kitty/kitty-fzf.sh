02c8ded16384d52531812bf44f3afcc224df5fda
Tronikel <contactdonatas@gmail.com>
2024-09-18 21:50:26 +0300

    update script a bit

--- synced/.config/kitty/kitty-fzf.sh
+++ synced/.config/kitty/kitty-fzf.sh
@@ -2,14 +2,11 @@
 
 all_tabs="$(
     kitty @ ls | jq -r '
-        .[]
-        | select(.is_active)
-        | .tabs[]
-        | select(.is_focused == false)
-        | [.title, "id:\(.id)"]
-        | @tsv
+        .[].tabs.[] |
+        [.title, .id] |
+        @tsv
     ' | column -ts $'\t'
 )"
 
 new_tab_id="$(fzf --header "Select tab:" --header-first --reverse <<<"${all_tabs}" | awk '{ print $2 }')"
-kitty @ focus-tab -m "${new_tab_id}"
+kitty @ focus-tab --match "id:${new_tab_id}"
