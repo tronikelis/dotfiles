b8b93015a77c55e421b2b9fc2a59b0f20a0b2157
Tronikel <contactdonatas@gmail.com>
2024-09-19 18:11:57 +0300

    fix kitty-fzf

--- synced/.config/kitty/kitty-fzf.sh
+++ synced/.config/kitty/kitty-fzf.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 all_tabs="$(
     kitty @ ls | jq -r '
@@ -8,5 +8,5 @@ all_tabs="$(
     ' | column -ts $'\t'
 )"
 
-new_tab_id="$(fzf --header "Select tab:" --header-first --reverse <<<"$all_tabs" | awk '{ print $2 }')"
+new_tab_id="$(fzf --header "Select tab:" --header-first --reverse <<<"$all_tabs" | awk '{ print $NF }')"
 kitty @ focus-tab --match "id:$new_tab_id"
