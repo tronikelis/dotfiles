0216cc021168789d6eaf850461ec2cff18d47245
Tronikel <contactdonatas@gmail.com>
2024-10-01 18:27:23 +0300

    hyprland screenshot tool keep focus

--- synced/.config/hypr/scripts/screenshot.sh
+++ synced/.config/hypr/scripts/screenshot.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+hyprpicker_id=-1
 
 tmpfile="$(mktemp)"
 
+hyprpicker -r -z &
+sleep 0.3
+hyprpicker_id=$!
+
 grim -g "$(slurp)" - | tee "$tmpfile" | wl-copy
+
+if [[ $hyprpicker_id != -1 ]]; then
+    kill $hyprpicker_id
+fi
 
 swappy -f - <"$tmpfile"
