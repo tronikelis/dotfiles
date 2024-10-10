af234d64209af5db50b836dc93a620c15c31d839
Tronikel <contactdonatas@gmail.com>
2024-10-10 17:45:14 +0300

    lower maxwidth

--- synced/.config/nvim/lua/plugins/lsp.lua
+++ synced/.config/nvim/lua/plugins/lsp.lua
@@ -198,7 +198,7 @@ return {
             }),
             formatting = {
                 format = require("lspkind").cmp_format({
-                    maxwidth = 50,
+                    maxwidth = 30,
                     ellipsis_char = "...",
                     -- shows details like auto import sources
                     show_labelDetails = true,
