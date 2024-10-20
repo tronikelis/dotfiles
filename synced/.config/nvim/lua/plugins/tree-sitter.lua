a01c5635fe122b1df69ddcc2cd6af7e2430e53c3
Tronikel <contactdonatas@gmail.com>
2024-10-20 11:12:29 +0300

    bump nvim

--- synced/.config/nvim/lua/plugins/tree-sitter.lua
+++ synced/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,8 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     dependencies = {
         {
             "nvim-treesitter/nvim-treesitter-context",
+            lazy = false,
             opts = {
                 -- try this out later
                 -- multiwindow = true,
