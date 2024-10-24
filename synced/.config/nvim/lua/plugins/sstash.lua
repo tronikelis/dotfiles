9bbbbcb04824124f1166789ad076a1dd7d8cc41a
Tronikel <contactdonatas@gmail.com>
2024-10-24 15:34:06 +0300

    dont write when leaving diff mode

--- synced/.config/nvim/lua/plugins/sstash.lua
+++ synced/.config/nvim/lua/plugins/sstash.lua
@@ -3,6 +3,10 @@ return {
     event = "VeryLazy",
     opts = {
         write_on_leave = function()
+            if vim.wo.diff then
+                return false
+            end
+
             local disabled_ft = { gitcommit = true, oil = true }
             return not disabled_ft[vim.bo.filetype]
         end,
