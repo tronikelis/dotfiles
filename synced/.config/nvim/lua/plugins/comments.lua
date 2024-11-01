c60745ad8766af7cc0d39ecc38360d1fa30c6c2f
Tronikel <contactdonatas@gmail.com>
2024-11-01 11:52:10 +0200

    change commenting plugin to folke's ts-comments.nvim

--- synced/.config/nvim/lua/plugins/comments.lua
+++ synced/.config/nvim/lua/plugins/comments.lua
@@ -1,16 +1,5 @@
 return {
-    "JoosepAlviste/nvim-ts-context-commentstring",
+    "folke/ts-comments.nvim",
+    opts = {},
     event = "VeryLazy",
-    config = function()
-        require("ts_context_commentstring").setup({
-            enable_autocmd = false,
-        })
-
-        local get_option = vim.filetype.get_option
-
-        vim.filetype.get_option = function(filetype, option)
-            return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
-                or get_option(filetype, option)
-        end
-    end,
 }
