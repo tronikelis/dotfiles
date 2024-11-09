3b785f6b283f0f23edcfba93185a5e95c7547c2f
Tronikel <contactdonatas@gmail.com>
2024-11-09 22:57:55 +0200

    fix(cspell): find correct config

--- synced/.config/nvim/lua/plugins/none-ls.lua
+++ synced/.config/nvim/lua/plugins/none-ls.lua
@@ -9,14 +9,21 @@ return {
         local none_ls = require("null-ls")
         local cspell = require("cspell")
 
+        local config = {
+            cspell_config_dirs = { "~/.config/nvim/" },
+        }
+
         none_ls.setup({
             sources = {
                 cspell.diagnostics.with({
                     diagnostics_postprocess = function(diagnostic)
                         diagnostic.severity = vim.diagnostic.severity.INFO
                     end,
+                    config = config,
                 }),
-                cspell.code_actions,
+                cspell.code_actions.with({
+                    config = config,
+                }),
             },
         })
     end,
