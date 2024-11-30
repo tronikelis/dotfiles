bfc5ece667c0c9bef0f89c2117163f149a76c5ec
Tronikel <contactdonatas@gmail.com>
2024-11-30 18:07:10 +0200

    feat: Bl user command

---
+++ synced/.config/nvim/lua/user_commands/bl.lua
@@ -0,0 +1,36 @@
+local cmds = {
+    -- force restart lsp
+    lsp = function()
+        local buf = vim.api.nvim_get_current_buf()
+        local function call(fn)
+            vim.api.nvim_buf_call(buf, fn)
+        end
+
+        call(vim.cmd.LspStop)
+        vim.defer_fn(function()
+            call(vim.cmd.LspStop)
+            vim.defer_fn(function()
+                call(vim.cmd.LspStart)
+            end, 1000)
+        end, 1000)
+    end,
+}
+
+vim.api.nvim_create_user_command(
+    "Bl",
+    vim.schedule_wrap(function(ev)
+        local cmd = cmds[ev.fargs[1]]
+        if not cmd then
+            print("what you entering man??")
+            return
+        end
+
+        cmd()
+    end),
+    {
+        nargs = 1,
+        complete = function()
+            return vim.tbl_keys(cmds)
+        end,
+    }
+)
