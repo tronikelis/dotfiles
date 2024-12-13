880b2f38cb91de8ce9e8595000439775c255c088
Tronikel <contactdonatas@gmail.com>
2024-12-13 18:09:04 +0200

    refactor: only show virtual_text with err/warn

--- synced/.config/nvim/lua/plugins/lsp.lua
+++ synced/.config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ vim.diagnostic.config({
     severity_sort = true,
     virtual_text = {
         source = true,
+        severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN },
     },
     float = {
         border = "rounded",
