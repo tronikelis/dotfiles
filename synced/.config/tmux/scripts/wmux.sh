2cd24ddb2525942d7efa44a1f14e9a4bee7e1099
Tronikel <contactdonatas@gmail.com>
2025-01-03 14:20:15 +0200

    feat: show full branch

--- synced/.config/tmux/scripts/wmux.sh
+++ synced/.config/tmux/scripts/wmux.sh
@@ -3,10 +3,11 @@
 source ~/.oh-my-zsh/custom/plugins/fzf-git/fzf-git.sh/fzf-git.sh
 
 wd="$(_fzf_git_worktrees --no-multi)"
+branch="$(cd "$wd" && git symbolic-ref --short HEAD || exit)"
 
 if [[ -z "$wd" ]]; then
     tmux display-message "empty selection"
     exit 0
 fi
 
-~/.config/tmux/scripts/upsert_session.sh "$wd"
+~/.config/tmux/scripts/upsert_session.sh "$wd" "w@$branch"
