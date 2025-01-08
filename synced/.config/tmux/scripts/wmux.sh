8094e9060eadd47d0fceeef262b21895ab61d2d7
Tronikel <contactdonatas@gmail.com>
2025-01-08 15:31:24 +0200

    feat: wmux, show root dir

--- synced/.config/tmux/scripts/wmux.sh
+++ synced/.config/tmux/scripts/wmux.sh
@@ -8,9 +8,24 @@ branch="$(
     git symbolic-ref --short HEAD
 )"
 
+root="$(
+    cd "$wd" || exit
+    while true; do
+        cd .. || exit
+        if [[ "$(pwd)" == "/" ]]; then
+            break
+        fi
+
+        if stat ".git" &>/dev/null; then
+            basename "$(pwd)"
+            break
+        fi
+    done
+)"
+
 if [[ -z "$wd" ]]; then
     tmux display-message "empty selection"
     exit 0
 fi
 
-~/.config/tmux/scripts/upsert_session.sh "$wd" "w@$branch"
+~/.config/tmux/scripts/upsert_session.sh "$wd" "$root@$branch"
