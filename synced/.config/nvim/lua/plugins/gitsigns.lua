08b08cdb32b08178855dfc023da9180a978e9df4
Tronikel <contactdonatas@gmail.com>
2025-01-15 18:12:40 +0200

    refactor: don't be fancy on blaming
    When I need to traverse history of blame I will just use
    git log -L lines

--- synced/.config/nvim/lua/plugins/gitsigns.lua
+++ synced/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,15 +1,8 @@
-local blame_fast = { "-w", "-C" }
-local blame_slow = { table.unpack(blame_fast), "-C", "-C" }
-
 return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
     opts = {
         current_line_blame = true,
-        current_line_blame_opts = {
-            extra_opts = blame_fast,
-        },
-
         on_attach = function(bufnr)
             local gitsigns = require("gitsigns")
             local opts = { buffer = bufnr }
@@ -18,10 +11,10 @@ return {
             vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, opts)
 
             vim.keymap.set("n", "<leader>hb", function()
-                gitsigns.blame_line({ extra_opts = blame_slow })
+                gitsigns.blame_line()
             end, opts)
             vim.keymap.set("n", "<leader>hB", function()
-                gitsigns.blame_line({ full = true, extra_opts = blame_slow })
+                gitsigns.blame_line({ full = true })
             end, opts)
 
             vim.keymap.set("n", "[h", function()
