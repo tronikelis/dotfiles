db8c13ea9777ec65fa2a47fab509c9422b714b6d
Tronikel <contactdonatas@gmail.com>
2025-01-16 23:09:01 +0200

    feat: 2 more autocmds

--- synced/.config/nvim/lua/remaps/init.lua
+++ synced/.config/nvim/lua/remaps/init.lua
@@ -108,6 +108,22 @@ function M.setup()
         end,
     })
 
+    -- restore cursor to file position in previous editing session
+    vim.api.nvim_create_autocmd("BufReadPost", {
+        callback = function(args)
+            local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+            local line_count = vim.api.nvim_buf_line_count(args.buf)
+            if mark[1] > 0 and mark[1] <= line_count then
+                vim.cmd('normal! g`"zz')
+            end
+        end,
+    })
+
+    -- auto resize splits when the terminal's window is resized
+    vim.api.nvim_create_autocmd("VimResized", {
+        command = "wincmd =",
+    })
+
     -- folding
     vim.opt.foldtext = ""
     vim.opt.foldcolumn = "0"
