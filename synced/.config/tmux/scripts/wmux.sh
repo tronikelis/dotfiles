c22832c0908984a34147660ec165bf0aed342797
Tronikel <contactdonatas@gmail.com>
2025-01-19 23:38:54 +0200

    refactor: wmux -> border-top

--- synced/.config/tmux/scripts/wmux.sh
+++ synced/.config/tmux/scripts/wmux.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 source ~/.oh-my-zsh/custom/plugins/fzf-git/fzf-git.sh/fzf-git.sh
 
-wd="$(_fzf_git_worktrees --no-multi --preview-window=bottom,50%,border)"
+wd="$(_fzf_git_worktrees --no-multi --preview-window=bottom,50%,border-top)"
 branch="$(
     cd "$wd" || exit
     git symbolic-ref --short HEAD
