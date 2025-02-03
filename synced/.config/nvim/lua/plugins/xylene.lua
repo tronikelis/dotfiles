5d4b3eabd0d7575b9bbc6ab10de798a6c316aee6
Donatas Bucinskas <donatas.bucinskas@vinted.com>
2025-02-03 10:23:28 +0200

    xylene keybinds

--- synced/.config/nvim/lua/plugins/xylene.lua
+++ synced/.config/nvim/lua/plugins/xylene.lua
@@ -7,12 +7,6 @@ return {
         "stevearc/oil.nvim",
     },
     opts = {
-        get_cwd = function()
-            if vim.bo.filetype == "oil" then
-                return require("oil").get_current_dir()
-            end
-            return vim.fn.getcwd()
-        end,
         get_current_file_dir = function()
             if vim.bo.filetype == "oil" then
                 return require("oil").get_current_dir()
@@ -20,13 +14,26 @@ return {
             return vim.fn.expand("%:p")
         end,
         on_attach = function(renderer)
-            vim.keymap.set("n", "<cr>", function()
-                renderer:toggle(vim.api.nvim_win_get_cursor(0)[1])
-            end, { buffer = renderer.buf })
+            local function map(mode, l, r)
+                vim.keymap.set(mode, l, r, { buffer = renderer.buf })
+            end
 
-            vim.keymap.set("n", "!", function()
+            map("n", "<cr>", function()
+                renderer:toggle(vim.api.nvim_win_get_cursor(0)[1])
+            end)
+
+            map("n", "!", function()
                 renderer:toggle_all(vim.api.nvim_win_get_cursor(0)[1])
-            end, { buffer = renderer.buf })
+            end)
+
+            map("n", "<leader>-", function()
+                local file = renderer:find_file_line(vim.api.nvim_win_get_cursor(0)[1])
+                if not file then
+                    return
+                end
+
+                require("oil").open(file.path)
+            end)
         end,
     },
 }
