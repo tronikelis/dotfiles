3273e494435fa183ff800acf94fe065889aed858
Tronikel <contactdonatas@gmail.com>
2025-02-11 09:00:19 +0200

    chore: change hl groups

--- synced/.config/nvim/lua/plugins/lsp.lua
+++ synced/.config/nvim/lua/plugins/lsp.lua
@@ -19,10 +19,10 @@ vim.diagnostic.config({
             [vim.diagnostic.severity.HINT] = "",
         },
         numhl = {
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
-            [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+            [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+            [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+            [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
         },
     },
 })
