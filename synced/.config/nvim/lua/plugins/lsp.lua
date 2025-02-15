9f8fa92438781d6be64502785dc9bbf15ba1b10a
Tronikel <contactdonatas@gmail.com>
2025-02-15 10:01:55 +0200

    refactor: no numhl for info/hint severities

--- synced/.config/nvim/lua/plugins/lsp.lua
+++ synced/.config/nvim/lua/plugins/lsp.lua
@@ -21,8 +21,8 @@ vim.diagnostic.config({
         numhl = {
             [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
             [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-            [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-            [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
         },
     },
 })
