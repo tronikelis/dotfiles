abd49df16f1a8572db382df22b06c4141bc95137
Tronikel <contactdonatas@gmail.com>
2025-02-15 09:51:46 +0200

    fix: use fzf default opts in zmux.sh

--- synced/.config/tmux/scripts/zmux.sh
+++ synced/.config/tmux/scripts/zmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wd="$(_ZO_FZF_OPTS="--no-sort --layout=reverse --tmux 70%,50%" zoxide query -i)"
+wd="$(_ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --no-sort --layout=reverse --tmux 70%,50%" zoxide query -i)"
 
 if [[ -z "$wd" ]]; then
     tmux display-message "empty selection"
