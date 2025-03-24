f2e2e21f12765e29ca602ce7e7b004bf0850cc61
Tronikel <contactdonatas@gmail.com>
2025-03-24 20:24:49 +0200

    chore: max lines 5

--- synced/.config/nvim/lua/plugins/tree-sitter.lua
+++ synced/.config/nvim/lua/plugins/tree-sitter.lua
@@ -7,7 +7,7 @@ return {
             lazy = false,
             opts = {
                 enable = true,
-                max_lines = 4,
+                max_lines = 5,
                 multiline_threshold = 1,
                 min_window_height = 25,
             },
