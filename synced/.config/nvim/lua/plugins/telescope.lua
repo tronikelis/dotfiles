3e40dbf04c64f23fda4b8eb63381ecc5bbb4c957
Tronikel <contactdonatas@gmail.com>
2025-03-29 11:15:12 +0200

    feat: lazy load telescope

--- synced/.config/nvim/lua/plugins/telescope.lua
+++ synced/.config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ local utils = require("utils")
 
 return {
     "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
