61603c2109db344c9f0f320912abcd1184750ccb
Tronikel <contactdonatas@gmail.com>
2025-04-15 17:08:14 +0300

    fix: Cdo/Cfdo run for all items in list

--- synced/.config/nvim/lua/user_commands/quickfix.lua
+++ synced/.config/nvim/lua/user_commands/quickfix.lua
@@ -7,14 +7,44 @@ end
 
 function M.setup()
     vim.api.nvim_create_user_command("Cfdo", function(ev)
-        vim.cmd.cfdo(ev.args)
+        vim.cmd.cfirst()
+
+        while true do
+            local ok, err = pcall(function()
+                vim.cmd(ev.args)
+            end)
+            if not ok then
+                vim.notify(tostring(err), vim.log.levels.ERROR)
+            end
+
+            if not pcall(function()
+                vim.cmd.cnfile()
+            end) then
+                break
+            end
+        end
     end, {
         preview = cmd_preview,
         nargs = "+",
     })
 
     vim.api.nvim_create_user_command("Cdo", function(ev)
-        vim.cmd.cdo(ev.args)
+        vim.cmd.cfirst()
+
+        while true do
+            local ok, err = pcall(function()
+                vim.cmd(ev.args)
+            end)
+            if not ok then
+                vim.notify(tostring(err), vim.log.levels.ERROR)
+            end
+
+            if not pcall(function()
+                vim.cmd.cnext()
+            end) then
+                break
+            end
+        end
     end, {
         preview = cmd_preview,
         nargs = "+",
