5a85bd15fd54e0f04733eb33011c1c48dc18eda0
Tronikel <contactdonatas@gmail.com>
2025-09-05 15:52:16 +0300

    refactor: telescope lazy load keymaps

--- synced/.config/nvim/plugin/_pack_telescope.lua
+++ synced/.config/nvim/plugin/_pack_telescope.lua
@@ -1,134 +1,3 @@
-local utils = require("utils")
-
-local actions = require("telescope.actions")
-local actions_state = require("telescope.actions.state")
-local lga_actions = require("telescope-live-grep-args.actions")
-
-local vimgrep_arguments = utils.flatten({ -- poor mans table copy, lmao
-    require("telescope.config").values.vimgrep_arguments,
-})
-
-table.insert(vimgrep_arguments, "--hidden")
-table.insert(vimgrep_arguments, "--trim")
-
-local picker_defaults = {
-    debounce = 200,
-}
-
-local with_picker_defaults = function(pickers)
-    for k, v in pairs(pickers) do
-        pickers[k] = vim.tbl_extend("force", picker_defaults, v)
-    end
-
-    return pickers
-end
-
-local copy_current_entry = function(prompt_bufnr)
-    local selected_entry = actions_state.get_selected_entry()
-    vim.fn.setreg("+", selected_entry.value)
-    actions.close(prompt_bufnr)
-end
-
-require("telescope").setup({
-    defaults = {
-        sorting_strategy = "ascending",
-        layout_strategy = "flex",
-        layout_config = {
-            height = 0.85,
-            width = 0.9,
-            horizontal = {
-                prompt_position = "top",
-            },
-            vertical = {
-                prompt_position = "top",
-                mirror = true,
-            },
-        },
-        vimgrep_arguments = vimgrep_arguments,
-        mappings = {
-            i = {
-                ["<c-h>"] = actions.preview_scrolling_left,
-                ["<c-l>"] = actions.preview_scrolling_right,
-
-                ["<c-y>"] = copy_current_entry,
-                ["<esc>"] = actions.close,
-
-                ["<c-f>"] = actions.cycle_previewers_next,
-                ["<c-b>"] = actions.cycle_previewers_prev,
-
-                ["<c-s>"] = actions.file_split,
-                ["<c-v>"] = actions.file_vsplit,
-            },
-        },
-        path_display = { "truncate" },
-        cache_picker = {
-            num_pickers = 50,
-            limit_entries = 500,
-        },
-        file_ignore_patterns = {
-            "^%.git/",
-        },
-    },
-    pickers = with_picker_defaults({
-        lsp_references = {
-            show_line = false,
-        },
-        lsp_definitions = {
-            show_line = false,
-        },
-        find_files = {
-            hidden = true,
-        },
-        buffers = {
-            sort_mru = true,
-            mappings = {
-                i = {
-                    ["<c-x>"] = actions.delete_buffer,
-                },
-            },
-        },
-        oldfiles = {
-            only_cwd = true,
-        },
-        git_status = {},
-        quickfix = {
-            trim_text = true,
-        },
-        help_tags = {},
-        current_buffer_fuzzy_find = {},
-        pickers = {},
-        git_bcommits = {},
-        git_bcommits_range = {},
-    }),
-    extensions = with_picker_defaults({
-        live_grep_args = {
-            mappings = {
-                i = {
-                    ["<c-space>"] = actions.to_fuzzy_refine,
-                    ["<c-f>"] = lga_actions.quote_prompt({ postfix = " -F" }),
-                },
-            },
-        },
-        git_diff_stat = {
-            preview_get_command = function(opts, entry)
-                return {
-                    "git",
-                    "-c",
-                    "delta.line-numbers=false",
-                    "diff",
-                    "-p",
-                    opts.git_args,
-                    "--",
-                    entry.absolute,
-                }
-            end,
-        },
-        ["ui-select"] = {
-            require("telescope.themes").get_cursor(),
-        },
-    }),
-})
-
 local function current_wd()
     if vim.bo.filetype == "oil" then
         return require("oil").get_current_dir()
@@ -136,59 +5,232 @@ local function current_wd()
     return vim.fn.expand("%:p:h")
 end
 
-require("telescope").load_extension("fzf")
-require("telescope").load_extension("ui-select")
-require("telescope").load_extension("live_grep_args")
-require("telescope").load_extension("git_diff_stat")
+local function setup()
+    if vim.g.did_telescope then
+        return
+    end
+    vim.g.did_telescope = true
 
-local builtin = require("telescope.builtin")
-local extensions = require("telescope").extensions
+    local utils = require("utils")
+    local actions = require("telescope.actions")
+    local actions_state = require("telescope.actions.state")
+    local lga_actions = require("telescope-live-grep-args.actions")
 
-local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+    local vimgrep_arguments = utils.flatten({ -- poor mans table copy, lmao
+        require("telescope.config").values.vimgrep_arguments,
+    })
 
-vim.keymap.set("n", "<leader>fg", extensions.live_grep_args.live_grep_args)
-vim.keymap.set("n", "<leader>fG", live_grep_args_shortcuts.grep_word_under_cursor)
+    table.insert(vimgrep_arguments, "--hidden")
+    table.insert(vimgrep_arguments, "--trim")
 
-vim.keymap.set("n", "<leader>gd", extensions.git_diff_stat.git_diff_stat)
-vim.keymap.set("n", "<leader>gs", builtin.git_status)
+    local picker_defaults = {
+        debounce = 200,
+    }
 
-vim.keymap.set("n", "<leader>of", builtin.oldfiles)
-vim.keymap.set("n", "<C-p>", builtin.find_files)
+    local with_picker_defaults = function(pickers)
+        for k, v in pairs(pickers) do
+            pickers[k] = vim.tbl_extend("force", picker_defaults, v)
+        end
 
-vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find)
-vim.keymap.set("n", "<leader>b", builtin.buffers)
-vim.keymap.set("n", "<leader>qf", builtin.quickfix)
+        return pickers
+    end
 
-vim.keymap.set("n", "<leader>ht", builtin.help_tags)
-vim.keymap.set("n", "<leader>op", builtin.pickers)
+    local copy_current_entry = function(prompt_bufnr)
+        local selected_entry = actions_state.get_selected_entry()
+        vim.fn.setreg("+", selected_entry.value)
+        actions.close(prompt_bufnr)
+    end
 
-vim.keymap.set("n", "<leader>fr", function()
-    builtin.find_files({ cwd = current_wd() })
+    require("telescope").setup({
+        defaults = {
+            sorting_strategy = "ascending",
+            layout_strategy = "flex",
+            layout_config = {
+                height = 0.85,
+                width = 0.9,
+                horizontal = {
+                    prompt_position = "top",
+                },
+                vertical = {
+                    prompt_position = "top",
+                    mirror = true,
+                },
+            },
+            vimgrep_arguments = vimgrep_arguments,
+            mappings = {
+                i = {
+                    ["<c-h>"] = actions.preview_scrolling_left,
+                    ["<c-l>"] = actions.preview_scrolling_right,
+
+                    ["<c-y>"] = copy_current_entry,
+                    ["<esc>"] = actions.close,
+
+                    ["<c-f>"] = actions.cycle_previewers_next,
+                    ["<c-b>"] = actions.cycle_previewers_prev,
+
+                    ["<c-s>"] = actions.file_split,
+                    ["<c-v>"] = actions.file_vsplit,
+                },
+            },
+            path_display = { "truncate" },
+            cache_picker = {
+                num_pickers = 50,
+                limit_entries = 500,
+            },
+            file_ignore_patterns = {
+                "^%.git/",
+            },
+        },
+        pickers = with_picker_defaults({
+            lsp_references = {
+                show_line = false,
+            },
+            lsp_definitions = {
+                show_line = false,
+            },
+            find_files = {
+                hidden = true,
+            },
+            buffers = {
+                sort_mru = true,
+                mappings = {
+                    i = {
+                        ["<c-x>"] = actions.delete_buffer,
+                    },
+                },
+            },
+            oldfiles = {
+                only_cwd = true,
+            },
+            git_status = {},
+            quickfix = {
+                trim_text = true,
+            },
+            help_tags = {},
+            current_buffer_fuzzy_find = {},
+            pickers = {},
+            git_bcommits = {},
+            git_bcommits_range = {},
+        }),
+        extensions = with_picker_defaults({
+            live_grep_args = {
+                mappings = {
+                    i = {
+                        ["<c-space>"] = actions.to_fuzzy_refine,
+                        ["<c-f>"] = lga_actions.quote_prompt({ postfix = " -F" }),
+                    },
+                },
+            },
+            git_diff_stat = {
+                preview_get_command = function(opts, entry)
+                    return {
+                        "git",
+                        "-c",
+                        "delta.line-numbers=false",
+                        "diff",
+                        "-p",
+                        opts.git_args,
+                        "--",
+                        entry.absolute,
+                    }
+                end,
+            },
+            ["ui-select"] = {
+                require("telescope.themes").get_cursor(),
+            },
+        }),
+    })
+
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("ui-select")
+    require("telescope").load_extension("live_grep_args")
+    require("telescope").load_extension("git_diff_stat")
+end
+
+local function keymap(mode, lhs, fn)
+    vim.keymap.set(mode, lhs, function()
+        setup()
+        fn()
+    end)
+end
+
+keymap("n", "<leader>fg", function()
+    require("telescope").extensions.live_grep_args.live_grep_args()
 end)
-vim.keymap.set("n", "<leader>fR", function()
-    extensions.live_grep_args.live_grep_args({ search_dirs = { current_wd() } })
+keymap("n", "<leader>fG", function()
+    require("telescope-live-grep-args.shortcuts").grep_word_under_cursor()
 end)
 
-vim.keymap.set({ "n", "v" }, "<leader>gc", function()
+keymap("n", "<leader>gd", function()
+    require("telescope").extensions.git_diff_stat.git_diff_stat()
+end)
+keymap("n", "<leader>gs", function()
+    require("telescope.builtin").git_status()
+end)
+
+keymap("n", "<leader>of", function()
+    require("telescope.builtin").oldfiles()
+end)
+keymap("n", "<C-p>", function()
+    require("telescope.builtin").find_files()
+end)
+
+keymap("n", "<leader>/", function()
+    require("telescope.builtin").current_buffer_fuzzy_find()
+end)
+keymap("n", "<leader>b", function()
+    require("telescope.builtin").buffers()
+end)
+keymap("n", "<leader>qf", function()
+    require("telescope.builtin").quickfix()
+end)
+
+keymap("n", "<leader>ht", function()
+    require("telescope.builtin").help_tags()
+end)
+keymap("n", "<leader>op", function()
+    require("telescope.builtin").pickers()
+end)
+
+keymap("n", "<leader>fr", function()
+    require("telescope.builtin").find_files({ cwd = current_wd() })
+end)
+keymap("n", "<leader>fR", function()
+    require("telescope").extensions.live_grep_args.live_grep_args({ search_dirs = { current_wd() } })
+end)
+
+keymap({ "n", "v" }, "<leader>gc", function()
     local mode = vim.api.nvim_get_mode().mode
     if mode == "n" then
-        builtin.git_bcommits()
+        require("telescope.builtin").git_bcommits()
     else
-        builtin.git_bcommits_range()
+        require("telescope.builtin").git_bcommits_range()
     end
-end, {})
+end)
 
-vim.keymap.set("n", "gd", builtin.lsp_definitions)
-vim.keymap.set("n", "gr", builtin.lsp_references)
-vim.keymap.set("n", "gt", builtin.lsp_type_definitions)
-vim.keymap.set("n", "gI", builtin.lsp_implementations)
-vim.keymap.set("n", "<leader>dc", function()
-    local severity = (vim.v.count == 0 and { nil } or { vim.v.count })[1]
-    builtin.diagnostics({ bufnr = 0, severity = severity })
+keymap("n", "gd", function()
+    require("telescope.builtin").lsp_definitions()
 end)
-vim.keymap.set("n", "<leader>dC", function()
-    local severity = (vim.v.count == 0 and { nil } or { vim.v.count })[1]
-    builtin.diagnostics({ severity = severity })
+keymap("n", "gr", function()
+    require("telescope.builtin").lsp_references()
 end)
-vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols)
-vim.keymap.set("n", "<leader>dS", builtin.lsp_workspace_symbols)
+keymap("n", "gt", function()
+    require("telescope.builtin").lsp_type_definitions()
+end)
+keymap("n", "gI", function()
+    require("telescope.builtin").lsp_implementations()
+end)
+keymap("n", "<leader>dc", function()
+    local severity = (vim.v.count == 0 and { nil } or { vim.v.count })[1]
+    require("telescope.builtin").diagnostics({ bufnr = 0, severity = severity })
+end)
+keymap("n", "<leader>dC", function()
+    local severity = (vim.v.count == 0 and { nil } or { vim.v.count })[1]
+    require("telescope.builtin").diagnostics({ severity = severity })
+end)
+keymap("n", "<leader>ds", function()
+    require("telescope.builtin").lsp_document_symbols()
+end)
+keymap("n", "<leader>dS", function()
+    require("telescope.builtin").lsp_workspace_symbols()
+end)
