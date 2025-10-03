962e3222edaa1d248214a53744fb58e76aeb3866
Tronikel <contactdonatas@gmail.com>
2025-10-03 13:28:58 +0300

    feat: git cherry-squash, better tmux switch session

--- synced/.config/tmux/scripts/fzf_switch_session.sh
+++ synced/.config/tmux/scripts/fzf_switch_session.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-tmux switch -t="$(tmux ls -F '#{session_name}' | fzf --tmux)"
+tmux switch -t="$(
+    tmux ls -F '#{session_name}|||#{session_last_attached}' \
+    | sort -n -t '|' -k 4 -r \
+    | column -t -s '|||' \
+    | fzf --no-sort --tmux \
+    | awk '{for(i=1;i<NF;++i)print $i}'
+)"
