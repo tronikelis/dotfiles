099cfa3363f081113ab48e43c5e0719ac5a1954e
Tronikel <contactdonatas@gmail.com>
2025-10-23 16:49:21 +0300

    feat: some useful scripts 2

--- synced/.config/tmux/scripts/fzf_switch_session.sh
+++ synced/.config/tmux/scripts/fzf_switch_session.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-tmux switch -t="$(
+session="$(
     tmux ls -F '#{session_name}|||#{session_last_attached}' \
     | sort -n -t '|' -k 4 -r \
     | column -t -s '|||' \
     | fzf --no-sort --tmux \
     | fextr 1 1
 )"
+
+if [[ ! "$session" ]]; then
+    tmux display-message "empty selection"
+    exit 0
+fi
+
+tmux switch -t="$session"
