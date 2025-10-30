29e2cd7f7e2a1b98d8e03ee169d660baad792457
Tronikel <contactdonatas@gmail.com>
2025-10-30 18:56:58 +0200

    feat: yank expand

---
+++ synced/.config/nvim/plugin/yank_expand.lua
@@ -0,0 +1,21 @@
+vim.api.nvim_create_user_command("YankExpand", function(ev)
+    local line = ""
+    if ev.count ~= -1 then
+        line = string.format("%d,%d:", ev.line1, ev.line2)
+    end
+
+    local flag = ev.fargs[1] or "%:~:."
+    if vim.bo.filetype == "oil" then
+        local dir = assert(require("oil").get_current_dir())
+        if flag:sub(1, 1) == "%" then
+            flag = flag:sub(2)
+        end
+        vim.fn.setreg("+", line .. vim.fn.fnamemodify(dir, flag))
+        return
+    end
+
+    vim.fn.setreg("+", line .. vim.fn.expand(flag))
+end, {
+    nargs = "?",
+    range = true,
+})
