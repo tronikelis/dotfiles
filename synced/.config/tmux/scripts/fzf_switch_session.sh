86108be647cbedc92df6617b8c9f4a3450bf4f1a
Tronikel <contactdonatas@gmail.com>
2025-11-04 15:46:17 +0200

    feat: fzf preview switch tmux session

--- synced/.config/tmux/scripts/fzf_switch_session.sh
+++ synced/.config/tmux/scripts/fzf_switch_session.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+fzf_preview="tmux ls -f \"#{m:\$(echo {} | fextr 1 1),#{session_name}}\""
+
 session="$(
     tmux ls -F '#{session_name}|||#{session_last_attached}' \
     | sort -n -t '|' -k 4 -r \
     | column -t -s '|||' \
-    | fzf --no-sort --tmux \
+    | fzf --preview-window down,1 --preview "$fzf_preview" --no-sort --tmux \
     | fextr 1 1
 )"
 
