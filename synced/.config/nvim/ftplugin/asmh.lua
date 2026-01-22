16055f10c858ca3eb6d600d2d3f1f3a6eba46f42
Tronikel <contactdonatas@gmail.com>
2026-01-22 15:23:04 +0200

    fix: undo_ftplugin

--- synced/.config/nvim/ftplugin/asmh.lua
+++ synced/.config/nvim/ftplugin/asmh.lua
@@ -1,3 +1,5 @@
 vim.opt_local.commentstring = "# %s"
 vim.treesitter.language.register("asm", "asmh")
 vim.keymap.set("i", "#", "X#", { buffer = 0 })
+
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or "") .. "\n setl commentstring<" .. "\n iunmap <buffer> #"
--- synced/.config/nvim/ftplugin/sd.lua
+++ synced/.config/nvim/ftplugin/sd.lua
@@ -1,2 +1,4 @@
 vim.opt_local.commentstring = "# %s"
 vim.keymap.set("i", "#", "X#", { buffer = 0 })
+
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or "") .. "\n setl commentstring<" .. "\n iunmap <buffer> #"
