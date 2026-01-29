abdc853fe869c9fec4ba592547415b0b4a815d57
Tronikel <contactdonatas@gmail.com>
2026-01-29 16:24:28 +0200

    fix: tmux open_editor.sh

--- synced/.config/tmux/scripts/open_editor.sh
+++ synced/.config/tmux/scripts/open_editor.sh
@@ -2,15 +2,33 @@
 
 set -eu
 
-function trim() {
-    echo "$1" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
-}
+filename="$(
+node - "$1" <<EOF
+    console.log((process.argv[2].split(":")[0] || "").trim())
+EOF
+)"
 
-filename="${1%:*}"
-line="${1#*:}"
+line="$(
+node - "$1" <<EOF
+    const colons = process.argv[2].split(":")
+    let line = ""
+    if (colons.length > 1) {
+        line = colons[1]
+    }
+    console.log((line || "").trim())
+EOF
+)"
 
-filename="$(trim "$filename")"
-line="$(trim "$line")"
+column="$(
+node - "$1" <<EOF
+    const colons = process.argv[2].split(":")
+    let line = ""
+    if (colons.length > 2) {
+        line = colons[2]
+    }
+    console.log((line || "").trim())
+EOF
+)"
 
 current_session="$(tmux display-message -p '#{session_id}')"
 pane_window="$(
@@ -22,15 +40,6 @@ pane_window="$(
 pane="$(echo "$pane_window" | awk '{print $1}')"
 window="$(echo "$pane_window" | awk '{print $2}')"
 
-cmd=":drop $filename"
-if [[ "$filename" == "$line" ]]; then
-    # no suffix found
-    line=""
-fi
-if [[ "$line" ]]; then
-    cmd="$cmd | $line"
-fi
-
 # open nvim if not already open
 if [[ ! "$pane" ]]; then
     tmux new-window -c "#{pane_current_path}"
@@ -41,7 +50,20 @@ fi
 tmux send-keys -t "$pane" -X cancel 2>/dev/null || true
 tmux send-keys -t "$pane" Escape
 
-tmux send-keys -t "$pane" "$cmd" Enter
+function send_cmd() {
+    tmux send-keys -t "$pane" ":$1" Enter
+}
+send_cmd "drop $filename"
+if [[ "$line" ]]; then
+    send_cmd "normal! ${line}G"
+fi
+if [[ "$column" ]]; then
+    send_cmd "normal! 0"
+    send_cmd "normal! ${column}l"
+    # go back one, because 1l should be the first column, but it actually moves the cursor
+    send_cmd "normal! 1h" 
+fi
+
 tmux select-window -t "$window"
 tmux select-pane -t "$pane" -Z;
 
