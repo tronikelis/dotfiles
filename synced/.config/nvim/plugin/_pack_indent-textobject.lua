b4697d17dbb7f871a09a713478d3c961e74e7e05
Tronikel <contactdonatas@gmail.com>
2026-02-09 19:50:59 +0200

    fix: indent textobject correctly map

--- synced/.config/nvim/plugin/_pack_indent-textobject.lua
+++ synced/.config/nvim/plugin/_pack_indent-textobject.lua
@@ -1,10 +1,13 @@
 ---@param key string
 local function map(key)
-    vim.keymap.set("v", "i" .. key, require("indent-textobject").select_inner)
-    vim.keymap.set("v", "a" .. key, require("indent-textobject").select_around)
+    local ikey = "i" .. key
+    local akey = "a" .. key
 
-    vim.keymap.set("o", "i" .. key, "<cmd>normal vii<cr>")
-    vim.keymap.set("o", "a" .. key, "<cmd>normal vai<cr>")
+    vim.keymap.set("v", ikey, require("indent-textobject").select_inner)
+    vim.keymap.set("v", akey, require("indent-textobject").select_around)
+
+    vim.keymap.set("o", "i" .. key, string.format("<cmd>normal v%s<cr>", ikey))
+    vim.keymap.set("o", "a" .. key, string.format("<cmd>normal v%s<cr>", akey))
 
     vim.keymap.set({ "n", "v" }, "[" .. key, require("indent-textobject").goto_inner_top)
     vim.keymap.set({ "n", "v" }, "]" .. key, require("indent-textobject").goto_inner_bot)
