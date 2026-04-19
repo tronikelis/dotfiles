afc6363e5d7a1e03b4f945fc797280497a43d079
Tronikel <contactdonatas@gmail.com>
2026-04-19 17:36:07 +0300

    feat: fzf_switch_session.sh show current pane command also

--- synced/.config/tmux/scripts/fzf_switch_session.sh
+++ synced/.config/tmux/scripts/fzf_switch_session.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-fzf_preview="tmux ls -f \"#{m:\$(echo {} | fextr 1 1),#{session_name}}\""
+fzf_preview="tmux ls -f \"#{m:\$(echo {} | fextr 1 2),#{session_name}}\""
 
 session="$(
-    tmux ls -F '#{session_name}|||#{session_last_attached}' \
-    | sort -n -t '|' -k 4 -r \
+    tmux ls -F '#{session_name}|||#{pane_current_command}|||#{session_last_attached}' \
+    | sort -n -t '|' -k 7 -r \
     | column -t -s '|||' \
     | fzf --preview-window down,1 --preview "$fzf_preview" --no-sort --tmux \
-    | fextr 1 1
+    | fextr 1 2
 )"
 
 if [[ ! "$session" ]]; then
