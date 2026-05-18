d75ca2805a7c046bfbff55ee3cdd2093a4e3c9d9
Tronikel <contactdonatas@gmail.com>
2026-05-18 16:34:25 +0300

    refactor: delete the autocommand after setupping

--- synced/.config/nvim/plugin/_pack_lazydev.lua
+++ synced/.config/nvim/plugin/_pack_lazydev.lua
@@ -4,15 +4,13 @@ vim.api.nvim_create_autocmd("FileType", {
     group = augroup,
     pattern = "lua",
     callback = function()
-        if not vim.g.did_lazydev then
-            require("lazydev").setup({
-                library = {
-                    -- See the configuration section for more details
-                    -- Load luvit types when the `vim.uv` word is found
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
-            })
-        end
-        vim.g.did_lazydev = true
+        require("lazydev").setup({
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        })
+        return true
     end,
 })
