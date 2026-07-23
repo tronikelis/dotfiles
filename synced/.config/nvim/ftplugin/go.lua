7c6ac4317405baae9dcc4aff49d653d305286c4d
Tronikel <contactdonatas@gmail.com>
2026-07-23 16:33:51 +0300

    nvim: user command GoTesthis

--- synced/.config/nvim/ftplugin/go.lua
+++ synced/.config/nvim/ftplugin/go.lua
@@ -1,2 +1,6 @@
 -- peak mapping
 vim.keymap.set("n", "<leader>ie", "oif err != nil {<cr>return err<cr>}<esc>", { buf = 0 })
+
+vim.api.nvim_buf_create_user_command(0, "GoTesthis", function()
+    vim.cmd.Terminal(string.format("go test ./%s", vim.fn.shellescape(vim.fn.expand("%:~:.:h"))))
+end, {})
